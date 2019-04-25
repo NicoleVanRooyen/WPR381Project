96cd6058cf18eba15268bf6c776f30883318adbd
evansta7 <lizy.vs@gmail.com>
2019-04-25 11:04:13 +0200

    Added server

--- Project2/client/src/App.js
+++ Project2/client/src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { apiResponse: ""};
+  }
+  callAPI(){
+    fetch('http://localhost:9000/testAPI')
+    .then(res=>res.text())
+    .then(res=>this.setState({apiResponse:res}));
+  }
+  componentWillMount(){
+    this.callAPI();
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,10 +30,11 @@ class App extends Component {
             <p>Enter a zipcode below to get the current weather for that area</p>
           </a>
           <img src={logo} className="App-logo" alt="logo" />
+          <p className="App-intro">{this.state.apiResponse}</p>
           <p className = "Enter-details">
             <form>
               <input type="text" name="postalcode" placeholder="Enter Postal Code"></input>
-              <input type="submit" value="ENTER"></input>
+              <input type="submit" value="ENTER" onClick="sendData()"></input>
             </form>
           </p>
         </header>
